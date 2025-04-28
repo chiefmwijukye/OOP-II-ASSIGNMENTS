3573aa7b50fbf243b60fc1b9a725af989fea6437
chiefmwijukye <chief.mwijukye@strathmore.edu>
2025-04-28 12:42:10 +0300

    OOP WORK

---
+++ codingchallenges/GoodeveningTillStop.java
@@ -0,0 +1,24 @@
+mport java.util.Scanner;
+
+//import static jdk.internal.org.jline.utils.Colors.s;
+
+public class GoodeveningTillStop {
+    static Scanner sc = new Scanner(System.in);
+
+    static void showGoodevening() {
+        String choice;
+
+        System.out.println("For Goodevening type g, anything else to stop");
+        choice = sc.next();
+
+        while ("g".equals(choice)){
+            System.out.println("Goodevening");
+            choice = sc.next();
+        }
+    }
+    public static void main(String[]args) {
+        GoodeveningTillStop obj = new GoodeveningTillStop();
+
+        GoodeveningTillStop.showGoodevening();
+    }
+}
